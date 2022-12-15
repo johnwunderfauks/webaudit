c5b6bec2ab8bd8f0ea7fb482bc7bf1e1a74dd526
johnwunderfauks <john@wunderfauks.com>
2022-12-15 12:10:08 +0800

    update

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const lighthouse = require("lighthouse");
 const { URL } = require("url");
 
 const { format } = require("date-fns");
-
+require('dotenv').config()
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
@@ -116,12 +116,12 @@ async function sendEmail(userEmail, userURL) {
 
     let transporter = nodemailer.createTransport({
       pool: true,
-      host: "mail.wunderfauks.com", //smtp.ethereal.email
+      host: process.env.EMAIL_HOST, //smtp.ethereal.email
       port: 465,
       secure: true, // true for 465, false for other ports
       auth: {
-        user: "seotest@wunderfauks.com", // generated ethereal user
-        pass: "31ibuF2%e?@c", // generated ethereal password
+        user: process.env.EMAIL_USER,"seotest@wunderfauks.com", // generated ethereal user
+        pass: process.env.EMAIL_PASSWORD, // generated ethereal password
       },
     });
 
