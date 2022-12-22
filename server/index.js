5f2c7b1d027b5b31f37c2fa50e597c6389d669ae
johnwunderfauks <john@wunderfauks.com>
2022-12-22 15:55:03 +0800

    test

--- server/index.js
+++ server/index.js
@@ -1,13 +1,55 @@
 require("dotenv").config({ path: `.env.local`, override: true });
-console.log(process.env.SEO_AUDIT_RESULTS_URL);
+
+//Code:
+var ln = require("ln");
+ln.PIPE_BUF = 512; //Set it in byte unit and based on the ulimit -a.
+//Default is 4096.
+//For example,
+//pipe size            (512 bytes, -p) 2
+//ln.PIPE_BUF = 512 * 2;
+//This controls the atomicity of the write operation.
+//Writes of {PIPE_BUF} or fewer bytes shall be atomic
+
+var appenders = [
+  {
+    level: "info", //Optional. It tells the appender what level should log.
+    //Default level will be "INFO" and "TRACE" if NODE_ENV=production and NODE_ENV=development respectively.
+    type: "file", //It defines the appender type. "file" is reserved and "console" is the default appender.
+    path: "[./log.]YYMMDDHHmm", //It defines the name and path of the log file.
+    //If you want to have log rotation, please define some tokens as a part of the filename.
+    //For the details and rules of tokens, you can take a look
+    //http://momentjs.com/docs/#/displaying/format/.
+    //Any chars inside [] will be escaped
+    //If you do not need the rotation,
+    //You can enclose the path with [] to be a static path, like "[./log]".
+    //Be aware of using [], static path is 400% faster than dynamic path.
+    isUTC: true, //Optional. It determines the tokens, "YYMMDDHHmm", is in UTC or local time
+    //Default is true.
+  },
+  {
+    level: "info",
+    type: "console", //It directly outputs to console.
+  },
+];
+var log = new ln({ name: "a", appenders: appenders });
+log.e("ln"); //Android-like logging signature:
+//log.trace = log.t
+//log.debug = log.d
+//log.info  = log.i
+//log.warn  = log.w
+//log.error = log.e
+//log.fatal = log.f
+// log.error(new Error("ln"));
+// log.e("ln", new Error("ln"), { a: true });
 const path = require("path");
 const express = require("express");
 const axios = require("axios");
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-
+const os = require("os");
 const puppeteer = require("puppeteer");
+const cluster = require("cluster");
+const totalCPUs = require("os").cpus().length;
 const lighthouse = require("lighthouse");
 const { URL } = require("url");
 
@@ -15,113 +57,310 @@ const { format } = require("date-fns");
 const nodemailer = require("nodemailer");
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
+const { currentLineHeight } = require("pdfkit");
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+if (cluster.isMaster) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
 
-app.get("/api", (req, res) => {
-  (async () => {
-    var currURL = "https://wunderfauks.com";
-    var currEmail = "contact@wunderfauks.com";
-    console.log(req.query.url);
+  // Fork workers.
+  for (let i = 0; i < totalCPUs / 2; i++) {
+    cluster.fork();
+  }
 
-    if (req.query.url) {
-      currURL = req.query.url;
-    }
-    if (req.query.email) {
-      currEmail = req.query.email;
-    }
+  cluster.once("message", (message) => {
+    console.log("worker thread done: ", message.id);
+  });
 
-    // Use Puppeteer to launch headful Chrome and don't use its default 800x600 viewport.
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-    });
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else if (cluster.isWorker) {
+  console.log(`Worker ${process.pid} started`);
+  const app = express();
+  // Have Node serve the files for our built React app
+  // app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.get("/api", (req, res) => {
+    (async () => {
+      var worker = cluster.worker;
+      var currURL = "-1";
+      var currEmail = "-1";
+      // log.e("worker received api request:", currURL, currEmail);
 
-    // Wait for Lighthouse to open url, then inject our stylesheet.
-    browser.on("targetchanged", async (target) => {
-      const page = await target.page();
-      if (page && page.url() === currURL) {
-        //await page.addStyleTag({content: '* {color: red}'});
-        //await sessionStorage.clear();
+      if (req.query.url) {
+        currURL = req.query.url;
       }
-    });
-
-    try {
-      // Lighthouse will open the URL.
-      // Puppeteer will observe `targetchanged` and inject our stylesheet.
-      const { lhr } = await lighthouse(currURL, {
-        port: new URL(browser.wsEndpoint()).port,
-        output: "json",
-        logLevel: "info",
-      });
-
-      //console.log();
-      //res.json(lhr);
-      // res.json(
-
-      // );
-      var lighthouseScores = `${Object.values(lhr.categories)
-        .map((c) => c.score)
-        .join(", ")}`;
-
-      //To do
-      //Save results in database
-      //Save PDF
-      //Trigger Email
-
-      var strapiData = {
-        data: {
-          date_created: format(new Date(), "yyyy-MM-dd'T'HH:mm:ssXX"),
-          site_email: currEmail,
-          site_url: currURL,
-          seo_result: lighthouseScores,
-        },
-      };
-      var strapiHeaders = {
-        headers: {
-          Authorization: process.env.SEO_AUDIT_KEY,
-        },
-      };
-      // Create a new PDF document
-      const doc = new PDFDocument();
-      var fileName = "output.pdf";
-      // Add some text and a rectangle
-      doc.text("SEO Scores for: " + currURL + "\n");
-      doc.text(lighthouseScores);
-
-      // Save the PDF to a file
-      doc.pipe(fs.createWriteStream("./uploads/" + fileName));
-      doc.end();
-
-      var strapiMsg = "";
-      const strapiResults = await axios
-        .post(process.env.SEO_AUDIT_RESULTS_URL, strapiData, strapiHeaders)
-        .then(function (response) {
-          console.log(response.data);
-          if (response.data) {
-            strapiMsg = "posted to strapi: " + response.data.id + "\n";
-            sendEmail(currEmail, currURL, fileName);
-          } else {
-          }
-        })
-        .catch(function (error) {
-          strapiMsg = "axios strapi error" + error;
-          console.log("strapi error ", error);
+      if (req.query.email) {
+        currEmail = req.query.email;
+      }
+      console.log("worker api request starting on:", currURL, currEmail);
+      try {
+        // Use Puppeteer to launch headful Chrome and don't use its default 800x600 viewport.
+        const browser = await puppeteer.launch({
+          headless: false,
+          defaultViewport: null,
         });
 
-      res.setHeader("Content-Type", "application/json");
-      res.json(`${strapiMsg}` + `:` + `${lighthouseScores}`);
-      res.end();
-    } catch (err) {
-      res.json(err.message);
-    } finally {
-      //res.json(`Done`);
-    }
+        // Lighthouse will open the URL.
+        // Puppeteer will observe `targetchanged` and inject our stylesheet.
+        const { lhr } = await lighthouse(currURL, {
+          port: new URL(browser.wsEndpoint()).port,
+          output: "json",
+          logLevel: "info",
+          chromeFlags: "ignore-certificate-errors",
+        });
+        var lighthouseScores = `${Object.values(lhr.categories)
+          .map((c) => c.score)
+          .join(", ")}`;
 
-    await browser.close();
-  })();
-});
+        // Send result back to main thread
+        process.send(currURL + "," + lighthouseScores);
+        //console.log("worker : ", lighthouseScores);
+        log.e("worker thread lighthouse done: ", lighthouseScores);
+
+        var strapiData = {
+          data: {
+            date_created: format(new Date(), "yyyy-MM-dd'T'HH:mm:ssXX"),
+            site_email: currEmail,
+            site_url: currURL,
+            seo_result: lighthouseScores,
+            initial_email: true,
+          },
+        };
+        var strapiHeaders = {
+          headers: {
+            Authorization: process.env.SEO_AUDIT_KEY,
+          },
+        };
+        // Create a new PDF document
+        const doc = new PDFDocument();
+        var fileName = generateRandomString(15) + ".pdf";
+        // Add some text and a rectangle
+        doc.text("SEO Scores for: " + currURL + "\n");
+        doc.text(lighthouseScores);
+
+        // Save the PDF to a file
+        doc.pipe(fs.createWriteStream("./uploads/" + fileName));
+        doc.end();
+
+        var strapiMsg = "";
+        const strapiResults = await axios
+          .post(process.env.SEO_AUDIT_RESULTS_URL, strapiData, strapiHeaders)
+          .then(function (response) {
+            console.log(response.data);
+            if (response.data) {
+              strapiMsg = "posted to strapi: " + response.data.data.id + "\n";
+              sendEmail(currEmail, currURL, fileName);
+            } else {
+            }
+            res.setHeader("Content-Type", "application/json");
+            res.json(`${strapiMsg}` + `:` + `${lighthouseScores}`);
+
+            browser.close();
+          })
+          .catch(function (error) {
+            strapiMsg = "axios strapi error" + error;
+            console.log("strapi error ", error);
+          });
+      } catch (err) {
+        // console.log("worker error: ", err);
+        log.e("worker error: ", err);
+        await browser.close();
+      }
+
+      // Send URL to be scraped to worker
+      // worker.send({ some: "data", arr: [currURL, currEmail] });
+
+      // // Receive result from worker and send it back to client
+      // worker.on("message", (result) => {
+      //   log.e("worker received scrape result:", result);
+      //   res.send(result);
+      // });
+
+      res.on("finish", () => {
+        // Code to clean up the child process goes here
+
+        process.exit();
+      });
+    })();
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+
+  //All other GET requests not handled before will return our React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+  // process.on("message", async (data) => {
+  //   log.e(`worker ${worker.process.pid} starting scrape`);
+  //   // Scrape URL using Lighthouse
+  //   try {
+  //     var currURL = data[0];
+  //     var currEmail = data[1];
+  //     log.e(currURL, currEmail);
+  //     const { lhr } = await lighthouse(currURL, {
+  //       port: new URL(currURL).port,
+  //       output: "json",
+  //       logLevel: "info",
+  //     });
+  //     var lighthouseScores = `${Object.values(lhr.categories)
+  //       .map((c) => c.score)
+  //       .join(", ")}`;
+
+  //     // Send result back to main thread
+  //     process.send(lighthouseScores);
+  //     //console.log("worker : ", lighthouseScores);
+  //     log.e("worker : ", lighthouseScores);
+  //   } catch (err) {
+  //     // console.log("worker error: ", err);
+  //     log.e("worker error: ", err);
+  //     process.exit();
+  //   }
+  //   // Terminate worker
+  //   process.exit();
+  // });
+}
+
+// app.get("/api2", (req, res) => {
+//   (async () => {
+//     var currURL = "https://wunderfauks.com";
+//     var currEmail = "contact@wunderfauks.com";
+//     console.log(req.query.url);
+
+//     if (req.query.url) {
+//       currURL = req.query.url;
+//     }
+//     if (req.query.email) {
+//       currEmail = req.query.email;
+//     }
+
+//     // Use Puppeteer to launch headful Chrome and don't use its default 800x600 viewport.
+//     const browser = await puppeteer.launch({
+//       headless: false,
+//       defaultViewport: null,
+//     });
+
+//     try {
+//       // Lighthouse will open the URL.
+//       // Puppeteer will observe `targetchanged` and inject our stylesheet.
+//       const { lhr } = await lighthouse(currURL, {
+//         port: new URL(browser.wsEndpoint()).port,
+//         output: "json",
+//         logLevel: "info",
+//       });
+
+//       //console.log();
+//       //res.json(lhr);
+//       // res.json(
+
+//       // );
+//       var lighthouseScores = `${Object.values(lhr.categories)
+//         .map((c) => c.score)
+//         .join(", ")}`;
+
+//       //To do
+//       //Save results in database
+//       //Save PDF
+//       //Trigger Email
+
+//       var strapiData = {
+//         data: {
+//           date_created: format(new Date(), "yyyy-MM-dd'T'HH:mm:ssXX"),
+//           site_email: currEmail,
+//           site_url: currURL,
+//           seo_result: lighthouseScores,
+//         },
+//       };
+//       var strapiHeaders = {
+//         headers: {
+//           Authorization: process.env.SEO_AUDIT_KEY,
+//         },
+//       };
+//       // Create a new PDF document
+//       const doc = new PDFDocument();
+//       var fileName = "output.pdf";
+//       // Add some text and a rectangle
+//       doc.text("SEO Scores for: " + currURL + "\n");
+//       doc.text(lighthouseScores);
+
+//       // Save the PDF to a file
+//       doc.pipe(fs.createWriteStream("./uploads/" + fileName));
+//       doc.end();
+
+//       var strapiMsg = "";
+//       const strapiResults = await axios
+//         .post(process.env.SEO_AUDIT_RESULTS_URL, strapiData, strapiHeaders)
+//         .then(function (response) {
+//           console.log(response.data);
+//           if (response.data) {
+//             strapiMsg = "posted to strapi: " + response.data.data.id + "\n";
+//             sendEmail(currEmail, currURL, fileName);
+//           } else {
+//           }
+//           res.setHeader("Content-Type", "application/json");
+//           res.json(`${strapiMsg}` + `:` + `${lighthouseScores}`);
+//           res.end();
+//         })
+//         .catch(function (error) {
+//           strapiMsg = "axios strapi error" + error;
+//           console.log("strapi error ", error);
+//         });
+//     } catch (err) {
+//       res.json(err.message);
+//     } finally {
+//       //res.json(`Done`);
+//     }
+
+//     await browser.close();
+//   })();
+// });
+
+// app.get("/api3", (req, res) => {
+//   (async () => {
+//     var currURL = "https://wunderfauks.com";
+//     var currEmail = "contact@wunderfauks.com";
+//     console.log(req.query.url);
+
+//     if (req.query.url) {
+//       currURL = req.query.url;
+//     }
+//     if (req.query.email) {
+//       currEmail = req.query.email;
+//     }
+
+//     scrapeLightHouse(res, currEmail, currURL);
+//   })();
+// });
+
+async function scrapeLightHouse(res, userEmail, userURL) {
+  // lighthouse(url, {
+  //   output: "html",
+  // }).then((results) => {
+  //   console.log(`Lighthouse results for ${url}:`);
+  //   console.log(results);
+  // });
+  try {
+    const { lhr } = await lighthouse(userURL, {
+      //  port: new URL(browser.wsEndpoint()).port,
+      output: "json",
+      logLevel: "info",
+    });
+    var lighthouseScores = `${Object.values(lhr.categories)
+      .map((c) => c.score)
+      .join(", ")}`;
+
+    var strapiMsg = "worker test";
+    res.setHeader("Content-Type", "application/json");
+    res.json(`${strapiMsg}` + `:` + `${lighthouseScores}`);
+    res.end();
+  } catch (err) {
+    res.json(err.message);
+  }
+}
 
 async function sendEmail(userEmail, userURL, pdf) {
   try {
@@ -174,11 +413,14 @@ async function sendEmail(userEmail, userURL, pdf) {
   }
 }
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+const generateRandomString = (myLength) => {
+  const chars =
+    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  const randomArray = Array.from(
+    { length: myLength },
+    (v, k) => chars[Math.floor(Math.random() * chars.length)]
+  );
 
-//All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+  const randomString = randomArray.join("");
+  return randomString;
+};
